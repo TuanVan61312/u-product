183367f39426e410ed11383e0b978d6bf7500b01
TuanVan61312 <92676025+TuanVan61312@users.noreply.github.com>
2022-06-07 05:37:21 +0700

    Add files via upload

---
+++ new.blade.php
@@ -0,0 +1,17 @@
+@extends('masters.giftshopMaster')
+
+@section('main')
+  <div class="container">
+    <br>
+    <div class="p-2 pt-md-5 pb-md-3 mx-auto ">
+      <h1 class="display-4">New Gifts</h1>
+    </div>
+    @include('partials.errors')
+
+    <form action="{{route('product.store')}}" method="post" enctype= "multipart/form-data">
+      @csrf
+      @include('giftshop.gifts.proFields')
+      <button type="submit" class="btn btn-dark">Submit</button>
+    </form>
+  </div>
+@endsection
